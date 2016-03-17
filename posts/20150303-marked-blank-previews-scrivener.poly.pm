088293e8875b99e32aedd10d83c4d7ee21eecd9b
Joel Dueck <joel@jdueck.net>
2016-03-17 12:48:48 -0500

    Fix typo in article

--- posts/20150303-marked-blank-previews-scrivener.poly.pm
+++ posts/20150303-marked-blank-previews-scrivener.poly.pm
@@ -8,7 +8,7 @@ You’re ◊emph{supposed} to be able to drag and drop a ◊link["http://www.lit
 
 However, when I did this on ◊emph{one particular Scrivener file}, the Marked preview came up blank -- even though I had plenty of Markdown-formatted content project.
 
-I finally found the solution, after checking other things◊numbered-note{Including whether “Include in compile” was turned on for each document within each document in the Scrivener project --- if it’s unchecked, Marked ◊emph{does} exclude it from the preview, even though no &quot;compile&quot; is actually taking place from Scrivener’s perspective.}. When you create a new “blank” Scrivener project, it gives you a couple of default folders; one of those is named ◊noun{Drafts} and contains a blank text document. That Drafts folder, it seems, is actually supposed to be the root folder of all your project’s content.
+I finally found the solution, after checking other things◊numbered-note{Including whether “Include in compile” was turned on for each document within each document in the Scrivener project --- if it’s unchecked, Marked ◊emph{does} exclude it from the preview, even though no “compile” is actually taking place from Scrivener’s perspective.}. When you create a new “blank” Scrivener project, it gives you a couple of default folders; one of those is named ◊noun{Drafts} and contains a blank text document. That Drafts folder, it seems, is actually supposed to be the root folder of all your project’s content.
 
 What I had done was create a folder outside the Drafts folder and create all of my content there. Marked will not preview content located outside the Drafts folder.
 
