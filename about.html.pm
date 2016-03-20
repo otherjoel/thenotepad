04b77a762af1a717c6ab35c3ef02ae394b8620d5
Joel Dueck <joel@jdueck.net>
2016-03-20 12:01:05 -0500

    Add About and Books pages

---
+++ about.html.pm
@@ -0,0 +1,11 @@
+#lang Pollen
+
+◊(define-meta title "About The Notepad")
+
+◊emph{The Notepad} is the successor to my old tech-notes blog “Notepad.Exe”. Originally it existed mainly for me to record solutions to unusual computer problems that crop up regularly in IT work, in hopes that anyone googling for help on the same problems in the future could find what they needed. Now that Stack Overflow exists that’s not as necesary as it used to be, but I still need a place for some kind of techie fish-wrap style blogging so here we are.
+
+The site is written with ◊link["//pollenpub.com"]{Pollen}, which means it’s basically a LISP program that spits out a website and a book from the same source◊numbered-note{I have written about this dual-publishing approach in the article ◊link["http://tilde.club/~joeld/secretary/posts/web-books.html"]{The Unbearable Lightness of Web Pages}.}. You can see ◊link["https://github.com/otherjoel/thenotepad"]{the source code on Github}.
+
+The fonts used on this site are ◊link["http://practicaltypography.com/charter.html"]{Charter} (free) for headings and body, and Triplicate (licensed from Matthew Butterick) for code.
+
+Previous incarnations of the site can be found at ◊link["http://notely.blogspot.com/2008/07/resurrected.html"]{blogspot.com} (2012) and ◊link["http://web.archive.org/web/20061105132534/http://notepadexe.blogspot.com/index.html"]{the Wayback Machine} (2006).
---
+++ books.html.pm
@@ -0,0 +1,5 @@
+#lang Pollen
+
+◊(define-meta title "Books to Read")
+
+These are some books you might want to read.
