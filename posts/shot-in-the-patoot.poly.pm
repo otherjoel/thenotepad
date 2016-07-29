ab6c540521497b2eaff1af91cae1057cbfe5ecda
Joel Dueck <joel@jdueck.net>
2016-07-28 20:36:47 -0500

    Add post ‘Shot in the Patoot’

---
+++ posts/shot-in-the-patoot.poly.pm
@@ -0,0 +1,17 @@
+#lang pollen
+
+◊(define-meta title "Shot in the Patoot")
+◊(define-meta published "2016-06-29")
+◊(define-meta topics "Tweets")
+
+◊tweet[#:id "535187560411447296"
+       #:handle "crushingbort"
+       #:realname "Dollars Horton"
+       #:permlink "https://twitter.com/crushingbort/status/535187560411447296"
+       #:timestamp "Nov 19 2014 3:47 PM"]{happy birthday to former President James Garfield, mortally shot in the patoot ◊image["tweet-535187560411447296.jpg"]}
+       
+◊tweet[#:id "535188461901582336"
+      #:handle "crushingbort"
+      #:realname "Dollars Horton"
+      #:permlink "https://twitter.com/crushingbort/status/535188461901582336"
+      #:timestamp "Nov 19 2014 3:50 PM"]{reminder that one time we shoved whiskey and beef bouillon up a president's butt until he died ◊link["http://www.nytimes.com/2006/07/25/health/25garf.html?pagewanted=all"]{nytimes.com/2006/07/25/hea…} ◊image["tweet-535188461901582336.png"]}
