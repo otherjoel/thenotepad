e85696e1fab0e898e7537cbad096d9372e698731
Joel Dueck <joel@jdueck.net>
2016-08-05 12:16:34 -0500

    Add some content to Books page

--- books.html.pm
+++ books.html.pm
@@ -1,5 +1,15 @@
 #lang Pollen
 
 ◊(define-meta title "Books to Read")
+◊(define-meta updated "2016-08-03")
 
-These are some books you might want to read.
+◊strong{"Best-of" Lists:}
+
+◊ul{
+    ◊item{◊link["/posts/50-books-every-child-should-read.html"]{50 Books Every Child Should Read} — picks from three of Britain’s leading children’s authors and two in-house book experts at the ◊emph{Independent}.}
+    ◊item{◊link["/posts/33-best-science-fiction-books.html"]{The 33 Best Science Fiction Novels Ever} collected from an Ask Reddit post.}
+}
+
+◊strong{Good Recent Reads}
+
+◊amazon["1455561789"]{◊image["book-beforethefall.jpg"]{Before the Fall}} ◊amazon["0547572484"]{◊image["book-maninhighcastle.jpg"]{Man in High Castle}}
--- posts/33-best-science-fiction-books.poly.pm
+++ posts/33-best-science-fiction-books.poly.pm
@@ -1,8 +1,8 @@
 #lang pollen
 
-◊(define-meta title "For Kindle: The 33 Best Science Fiction Novels Ever")
+◊(define-meta title "The 33 Best Science Fiction Novels Ever")
 ◊(define-meta published "2011-09-14")
-◊(define-meta topics "Kindle,ebooks,books")
+◊(define-meta topics "books")
 
 Another “best of” list for your Kindle, brought to you this time by ◊link["http://www.reddit.com/r/AskReddit/comments/k70dh/whats_the_best_scifi_novel_you_have_ever_read/"]{this Ask Reddit post}.
 
