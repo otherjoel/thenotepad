0e449c36ab832b2099e587a9f2663dd6b139e7a9
Matthew Butterick <mb@mbtype.com>
2016-11-12 09:13:29 -0800

    alternate approach to name prefixing

--- pollen.rkt
+++ pollen.rkt
@@ -6,74 +6,43 @@
          txexpr
          pollen/tag         ; default-tag-function
          "util-date.rkt"
-         "util-template.rkt"
-         "pollen-local/polytag.rkt"
-         "pollen-local/tags-html.rkt"
-         "pollen-local/tags-pdf.rkt")
+         "util-template.rkt")
 
 (provide string-split
-         (all-from-out "util-date.rkt" "util-template.rkt")
-         (all-from-out "pollen-local/tags-pdf.rkt"))
+         (all-from-out "util-date.rkt" "util-template.rkt"))
 (provide (all-defined-out))
 
 (module setup racket/base
-    (provide (all-defined-out))
-    (define poly-targets '(html pdf)))
+  (provide (all-defined-out))
+  (define poly-targets '(html pdf)))
 
-(poly-branch-tag root)
-    
-(poly-branch-tag p)
-(poly-branch-tag i)
-(poly-branch-tag emph)
-(poly-branch-tag b)
-(poly-branch-tag strong)
-(poly-branch-tag color c)
-(poly-branch-tag ol)
-(poly-branch-tag ul)
-(poly-branch-tag item)
-(poly-branch-tag sup)
-(poly-branch-tag link url)
-(poly-branch-tag blockquote)
-(poly-branch-tag newthought)
-(poly-branch-tag smallcaps)
-(poly-branch-tag inline-math)
-(poly-branch-tag center)
+(require (for-syntax racket/syntax syntax/stx))
+(define-syntax (define-poly-tag-from-file stx)
+  (syntax-case stx ()
+    [(_ ID [TARGET SOURCE] ...)
+     (with-syntax* ([(TARGET: ...) (stx-map
+                                    (λ(stx) (format-id stx "~a:" stx))
+                                    #'(TARGET ...))]
+                    [(TARGET:ID ...) (stx-map
+                                             (λ(stx) (format-id stx "~a~a" stx #'ID))
+                                             #'(TARGET: ...))])
+       #'(begin
+           (require (prefix-in TARGET: SOURCE)) ...
+           (provide ID)
+           (define-tag-function (ID attrs elems)
+             (case (current-poly-target)
+               [(TARGET) (TARGET:ID attrs elems)] ...))))]))
 
-(poly-branch-tag section)
-(poly-branch-tag subsection)
+;; basic form
+(define-poly-tag-from-file root
+  [html "pollen-local/tags-html.rkt"]
+  [pdf "pollen-local/tags-pdf.rkt"])
 
-(poly-branch-tag index-entry entry)
-(poly-branch-tag figure src (fullwidth #f) (has-print-version #f))
-(poly-branch-tag margin-figure src (has-print-version #f))
-(poly-branch-tag image src (has-print-version #f))
+;; shorthand for other tags
+(define-syntax-rule (my-poly-tags ID ...)
+  (begin
+    (define-poly-tag-from-file ID
+      [html "pollen-local/tags-html.rkt"]
+      [pdf "pollen-local/tags-pdf.rkt"]) ...))
 
-(poly-branch-tag margin-note)
-(poly-branch-tag numbered-note)
-
-; Note that because of the need to escape backslashes in LaTeX, you
-; cannot use any other commands inside a ◊code tag
-(poly-branch-tag code)
-(poly-branch-tag noun)
-(poly-branch-tag blockcode (filename ""))
-
-(poly-branch-tag Latex)
-(poly-branch-tag verse (title "") (italic #f))
-
-#|
-  ◊tweet and ◊retweet handle markup for embedded tweets. I use a Python script
-  to generate the tags in my Pollen markup (github.com/otherjoel/blackbirdpy).
-|#
-(poly-branch-tag tweet)
-(poly-branch-tag retweet)
-
-(poly-branch-tag updatebox date-str)
-(poly-branch-tag comment (authorlink ""))
-
-; Our ◊table will support an optional argument specifying the text alignment for
-; each column in the table: E.g. "llrc" means two left-aligned columns, a right-
-; aligned column, and a center-aligned column (4 columns total).
-(poly-branch-tag table (columns #f))
-      
-(define (amazon product-id . contents)
-  (define affiliate-id "thloya-20")
-  (apply link (format "https://amzn.com/~a/?tag=~a" product-id affiliate-id) contents))
+(my-poly-tags p i emph b strong code)
