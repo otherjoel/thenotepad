ac0c6d1e266b53e59afa41448950949854701a2c
Joel Dueck <joel@jdueck.net>
2017-08-13 22:19:03 -0500

    New Post: A Screen

---
+++ posts/it-overhaul-phase-zero-a-screen.poly.pm
@@ -0,0 +1,22 @@
+#lang pollen
+
+◊(define-meta title "Home IT Overhaul, Phase Zero: A Screen")
+◊(define-meta published "2017-08-13")
+◊(define-meta topics "Home IT Overhaul")
+
+My home network has had a pretty basic setup for the last six years. My wife and I each have a laptop, connected to the internet with an Asus wifi router and a cable modem. And we have a wifi B&W laser printer. That’s it.
+
+Since we finished the basement and installed a 55″ TV, however, I’ve had my eye on some drastic improvements and additions to our home’s IT capabilities. I will outline the overall plan in another post, however, to get things rolling I thought I’d just write about the first small step in that plan, which I took today: I bought a monitor.
+
+◊figure["coby-monitor.jpg"]{A Coby TFTV1525 15″ monitor/TV, purchased used, missing remote}
+
+I have almost zero spare computer parts lying around my house, which is surely strange for an IT manager. I’ve made a point of getting rid of stuff I don’t need, which means I get to start from scratch.
+
+This tiny used monitor will sit on my new IT “rack” when I need a direct-attached display for setting up or troubleshooting servers. It’s perfect for my setup for several reasons:
+
+◊ol{
+◊item{It’s small}
+◊item{It’s cheap ($20 on Craigslist)}
+◊item{It has both HDMI and VGA inputs, so it will work with any computer, old or new, without any converters}
+◊item{It also has a TV tuner, so I can use it to watch broadcast television on the rare occasions where that might come in handy (weather emergencies, etc)}
+}
